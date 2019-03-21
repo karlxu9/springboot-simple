d57d5c91f866212a38def838be8391c75ee32385
子路 <gitosc123>
2019-03-21 15:52:38 +0800

    first commit

---
+++ spring-boot-mybatis-annotation/src/main/java/com/neo/mybatis/MybatisApplication.java
@@ -0,0 +1,15 @@
+package com.neo.mybatis;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.neo.mybatis.mapper")
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+
+}
